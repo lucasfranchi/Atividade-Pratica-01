97948f029ffb3e748f7f0f0efdedb6f8ed0ff314
Lucas Franchi <lucas.franchi12@outlook.com>
2022-05-23 19:14:11 -0300

    Realizando a atividade pratica

---
+++ src/app/Interfaces/cadastro.ts
@@ -0,0 +1,8 @@
+/* Interface criada para padronizar os cadastros e declarar o que todos devem ter */
+
+export interface CadastroInterface {
+  nome: string
+  ru: number
+  nomeDoCurso: string
+  dataAniversario: string
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { CadastroInterface } from './Interfaces/cadastro';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exercicio';
+  /* Variavel para o titulo da pagina */
+  tituloDaPagina = 'Agenda'
+
+  /* Variavel que recebe um array com os cadastros do tipo criado */
+  cadastros: Array<CadastroInterface> = [{
+      nome: 'Lucas Franchi',
+      ru: 3638107,
+      nomeDoCurso: 'Engenharia de Software',
+      dataAniversario: this.dateToString(new Date('02/12/2003'))
+    },
+    {
+      nome: 'José',
+      ru: 3638102,
+      nomeDoCurso: 'Matemática',
+      dataAniversario: this.dateToString(new Date('10/18/2004'))
+    },
+    {
+      nome: 'Antonio',
+      ru: 3638523,
+      nomeDoCurso: 'Engenharia Civil',
+      dataAniversario: this.dateToString(new Date('07/13/2002'))
+    },
+    {
+      nome: 'Rebeca',
+      ru: 3638643,
+      nomeDoCurso: 'Enfermagem',
+      dataAniversario: this.dateToString(new Date('11/24/2004'))
+    },
+    {
+      nome: 'Carla',
+      ru: 3638967,
+      nomeDoCurso: 'Arquitetura',
+      dataAniversario: this.dateToString(new Date('02/23/2000'))
+    }
+  ]
+
+  /* Declaracao no construtor para realizar a dormatacao da data */
+  constructor(private datePipe: DatePipe) {}
+
+  /* Metodo criado para devolver uma string formatada da data para a variavel */
+  dateToString(data: Date): string {
+    const dataParaString = this.datePipe.transform(data, 'dd/MM/yyyy');
+    return dataParaString
+  }
+
 }
+
+
